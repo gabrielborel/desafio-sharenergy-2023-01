0405d1ecf458e98c0732dd127035ec07ba01207e
Gabriel Borel <biel_borel@hotmail.com>
2023-01-03 18:05:15 -0300

    schema de cliente criado

---
+++ backend-node/src/modules/clients/schemas/client.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const clientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  cpf: { type: String, required: true },
+  cellphone: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const clientModel = mongoose.model("Client", clientSchema);
