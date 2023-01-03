7b4a0bde509148158e44e9a5bdc7a79ed9089d93
Gabriel Borel <biel_borel@hotmail.com>
2023-01-03 20:06:05 -0300

    rota de listagem de usuário por id criado

--- backend-node/src/shared/routes/clients.routes.ts
+++ backend-node/src/shared/routes/clients.routes.ts
@@ -1,6 +1,7 @@
 import Router from "express";
 import { CreateClientController } from "../../modules/clients/use-cases/create-client/create-client-controller";
 import { FindAllClientsController } from "../../modules/clients/use-cases/find-all-clients/find-all-clients-controller";
+import { FindClientByIdController } from "../../modules/clients/use-cases/find-client-by-id/find-client-by-id-controller";
 import { createClientSchema } from "../../modules/clients/validators/create-client-schema-validator";
 import { validate } from "../middlewares/validate-client-schema-middleware";
 
@@ -8,6 +9,7 @@ export const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
 const findAllClientsController = new FindAllClientsController();
+const findClientByIdController = new FindClientByIdController();
 
 clientsRoutes.post(
   "/",
@@ -16,3 +18,5 @@ clientsRoutes.post(
 );
 
 clientsRoutes.get("/", findAllClientsController.handle);
+
+clientsRoutes.get("/:id", findClientByIdController.handle);
