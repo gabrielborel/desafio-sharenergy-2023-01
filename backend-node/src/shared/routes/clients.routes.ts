1a56570fd82d057a8ff50f8eb499a5904448e64a
Gabriel Borel <biel_borel@hotmail.com>
2023-01-03 18:48:15 -0300

    middleware de validação adicionao a rota de criação de cliente

--- backend-node/src/shared/routes/clients.routes.ts
+++ backend-node/src/shared/routes/clients.routes.ts
@@ -1,8 +1,10 @@
 import Router from "express";
 import { CreateClientController } from "../../modules/clients/use-cases/create-client/create-client-controller";
+import { clientSchema } from "../../modules/clients/validators/client-schema-validator";
+import { validate } from "../middlewares/validate-client-schema-middleware";
 
 export const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
 
-clientsRoutes.post("/", createClientController.handle);
+clientsRoutes.post("/", validate(clientSchema), createClientController.handle);
