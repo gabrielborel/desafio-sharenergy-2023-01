6c035574a423b4db0ab5c1e319a228646922d4d5
Gabriel Borel <biel_borel@hotmail.com>
2023-01-03 19:37:57 -0300

    import fix

--- backend-node/src/shared/routes/clients.routes.ts
+++ backend-node/src/shared/routes/clients.routes.ts
@@ -1,6 +1,6 @@
 import Router from "express";
 import { CreateClientController } from "../../modules/clients/use-cases/create-client/create-client-controller";
-import { createClientSchema } from "../../modules/clients/validators/client-schema-validator";
+import { createClientSchema } from "../../modules/clients/validators/create-client-schema-validator";
 import { validate } from "../middlewares/validate-client-schema-middleware";
 
 export const clientsRoutes = Router();
