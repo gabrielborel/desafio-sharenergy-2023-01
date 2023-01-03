23287940c3dd6510ae0bdd303946da8d59221581
Gabriel Borel <biel_borel@hotmail.com>
2023-01-03 19:01:16 -0300

    mudança no nome do arquivo de schema de validação de criação de usuário

---
+++ backend-node/src/modules/clients/validators/create-client-schema-validator.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const createClientSchema = z.object({
+  body: z.object({
+    name: z
+      .string({ required_error: "Nome é obrigatório." })
+      .min(5, "O nome deve ter no mínimo 5 caracteres.")
+      .max(30, "O nome deve ter no máximo 30 caracteres."),
+    email: z
+      .string({ required_error: "Email é obrigatório." })
+      .email("Email inválido."),
+    cellphone: z
+      .string({ required_error: "Telefone é obrigatório." })
+      .min(9, "O telefone deve ter no mínimo 9 dígitos.")
+      .max(11, "O telefone deve ter no máximo 11 dígitos."),
+    cpf: z
+      .string({ required_error: "CPF é obrigatório." })
+      .min(11, "O telefone deve ter no mínimo 11 dígitos.")
+      .max(12, "O telefone deve ter no máximo 12 dígitos."),
+    address: z
+      .string({ required_error: "Endereço é obrigatório." })
+      .min(20, "O endereço deve ter no mínimo 20 dígitos.")
+      .max(70, "O endereço deve ter no máximo 70 dígitos."),
+  }),
+});
--- backend-node/src/shared/routes/clients.routes.ts
+++ backend-node/src/shared/routes/clients.routes.ts
@@ -1,10 +1,14 @@
 import Router from "express";
 import { CreateClientController } from "../../modules/clients/use-cases/create-client/create-client-controller";
-import { clientSchema } from "../../modules/clients/validators/client-schema-validator";
+import { createClientSchema } from "../../modules/clients/validators/client-schema-validator";
 import { validate } from "../middlewares/validate-client-schema-middleware";
 
 export const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
 
-clientsRoutes.post("/", validate(clientSchema), createClientController.handle);
+clientsRoutes.post(
+  "/",
+  validate(createClientSchema),
+  createClientController.handle
+);
