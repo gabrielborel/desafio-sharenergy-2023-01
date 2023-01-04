ff7d9d8ac052c4b3e07c1c2f00857c0f1a46efd9
Gabriel Borel <biel_borel@hotmail.com>
2023-01-04 02:53:01 -0300

    rotas de autenticação adicionadas

--- backend-node/src/shared/routes/index.routes.ts
+++ backend-node/src/shared/routes/index.routes.ts
@@ -1,6 +1,9 @@
 import Router from "express";
+import { ensureAuthenticated } from "../middlewares/ensure-authenticated";
+import { authenticateRoutes } from "./authenticate.routes";
 import { clientsRoutes } from "./clients.routes";
 
 export const routes = Router();
 
 routes.use("/clients", clientsRoutes);
+routes.use("/authenticate", authenticateRoutes);
