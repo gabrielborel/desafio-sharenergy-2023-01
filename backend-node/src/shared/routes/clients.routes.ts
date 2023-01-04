32a9e30829f3f2e9f3df9ff53e3b65c55a0b8239
Gabriel Borel <biel_borel@hotmail.com>
2023-01-04 02:52:45 -0300

    adicionado middleware de autenticação em todas as rotas de cliente

--- backend-node/src/shared/routes/clients.routes.ts
+++ backend-node/src/shared/routes/clients.routes.ts
@@ -7,6 +7,7 @@ import { FindClientByIdController } from "../../modules/clients/use-cases/find-c
 import { createClientSchema } from "../../modules/clients/validators/create-client-schema-validator";
 import { validate } from "../middlewares/validate-client-schema-middleware";
 import { updateClientSchema } from "../../modules/clients/validators/update-client-schema-validator";
+import { ensureAuthenticated } from "../middlewares/ensure-authenticated";
 
 export const clientsRoutes = Router();
 
@@ -18,18 +19,24 @@ const updateClientController = new UpdateClientController();
 
 clientsRoutes.post(
   "/",
+  ensureAuthenticated,
   validate(createClientSchema),
   createClientController.handle
 );
 
-clientsRoutes.get("/", findAllClientsController.handle);
+clientsRoutes.get("/", ensureAuthenticated, findAllClientsController.handle);
 
-clientsRoutes.get("/:id", findClientByIdController.handle);
+clientsRoutes.get("/:id", ensureAuthenticated, findClientByIdController.handle);
 
-clientsRoutes.delete("/:id", deleteClientController.handle);
+clientsRoutes.delete(
+  "/:id",
+  ensureAuthenticated,
+  deleteClientController.handle
+);
 
 clientsRoutes.put(
   "/:id",
+  ensureAuthenticated,
   validate(updateClientSchema),
   updateClientController.handle
 );
