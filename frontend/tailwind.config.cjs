bbf9788227f3d32a39fe0daf12ce286b31791538
Gabriel Borel <biel_borel@hotmail.com>
2023-01-04 11:28:27 -0300

    adicionado flowbite, react-hook-form, zod, axios, classnames e @radix-ui/react-toasts

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,7 +6,17 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          500: "#1BA2A1",
+          600: "#189186",
+          700: "#158077",
+          800: "#105f58",
+          900: "#0d4e48",
+        },
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
