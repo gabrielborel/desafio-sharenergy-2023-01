9a24962d0eaf6b10fac4b85ad63de203130fe7dd
Gabriel Borel <biel_borel@hotmail.com>
2023-01-04 19:45:43 -0300

    componente de navegação para dipositivos menores criado

---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,51 @@
+import * as DM from "@radix-ui/react-dropdown-menu";
+import classNames from "classnames";
+import { List } from "phosphor-react";
+import { Link, useLocation } from "react-router-dom";
+
+export function Menu() {
+  const location = useLocation();
+
+  return (
+    <DM.Root>
+      <DM.Trigger asChild className="md:hidden">
+        <button className="hover:text-green-500 transition-colors">
+          <List size={34} />
+        </button>
+      </DM.Trigger>
+
+      <DM.Portal>
+        <DM.Content className="md:hidden min-w-[150px] p-3 rounded-md bg-white shadow-md">
+          <DM.Item
+            className={classNames({
+              "text-green-600": location.pathname === "/",
+            })}
+          >
+            <Link to="/">Random Users</Link>
+          </DM.Item>
+          <DM.Item
+            className={classNames({
+              "text-green-600": location.pathname === "/cats",
+            })}
+          >
+            <Link to="/cats">Http Cats</Link>
+          </DM.Item>
+          <DM.Item
+            className={classNames({
+              "text-green-600": location.pathname === "/dogs",
+            })}
+          >
+            <Link to="/dogs">Random Dogs</Link>
+          </DM.Item>
+          <DM.Item
+            className={classNames({
+              "text-green-600": location.pathname === "/clients",
+            })}
+          >
+            <Link to="/clients">Clients</Link>
+          </DM.Item>
+        </DM.Content>
+      </DM.Portal>
+    </DM.Root>
+  );
+}
