296d76c4fdd085a4fa3f3e4df4c70e67fbed1a0f
Gabriel Borel <biel_borel@hotmail.com>
2023-01-04 19:45:54 -0300

    componente header finalizado

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Menu } from "./Menu";
+import { Navbar } from "./Navbar";
+
+export function Header() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    setLoading(true);
+    localStorage.removeItem("@sharenergy-access_token");
+    sessionStorage.removeItem("@sharenergy-access_token");
+
+    setTimeout(() => {
+      navigate("/auth");
+    }, 1500);
+  }
+
+  return (
+    <header className="bg-gray-100 py-6 px-3">
+      <section className="max-w-[1220px] items-center flex justify-between mx-auto">
+        <div>
+          <Menu />
+          <Navbar />
+        </div>
+
+        <button
+          onClick={handleLogout}
+          type="button"
+          disabled={loading}
+          className="text-red-700 disabled:opacity-50 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-md text-sm px-5 py-1.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+        >
+          Sair da conta
+        </button>
+      </section>
+    </header>
+  );
+}
