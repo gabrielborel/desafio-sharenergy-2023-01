88069483918e792bd6b9e07e3781574f26f60960
Gabriel Borel <biel_borel@hotmail.com>
2023-01-07 19:46:59 -0300

    rotas de gato e cachorro adicionadas

--- backend-node/src/shared/routes/index.routes.ts
+++ backend-node/src/shared/routes/index.routes.ts
@@ -1,9 +1,12 @@
 import Router from "express";
-import { ensureAuthenticated } from "../middlewares/ensure-authenticated";
 import { authenticateRoutes } from "./authenticate.routes";
+import { catsRoutes } from "./cats.routes";
 import { clientsRoutes } from "./clients.routes";
+import { dogsRoutes } from "./dogs.routes";
 
 export const routes = Router();
 
 routes.use("/clients", clientsRoutes);
 routes.use("/authenticate", authenticateRoutes);
+routes.use("/cats", catsRoutes);
+routes.use("/dogs", dogsRoutes);
