fb91feede61d85b383f0f89baffae6b17e37db13
Gabriel Borel <biel_borel@hotmail.com>
2023-01-07 19:53:53 -0300

    adicionado middleware de cors

--- backend-node/src/app.ts
+++ backend-node/src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import "./database/mongoClient";
 import "./shared/containers";
 import { routes } from "./shared/routes/index.routes";
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
