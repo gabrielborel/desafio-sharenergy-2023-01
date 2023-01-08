2143110eb86f8daf72a3375b2e280c1e85e8369c
Gabriel Borel <biel_borel@hotmail.com>
2023-01-08 20:56:57 -0300

    rotas de criar cliente e procurar por id adicionado

---
+++ backend-go/internal/api/clients.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gabrielborel/desafio-sharenergy-2023-01/backend-go/pkg/models"
+	"github.com/labstack/echo/v4"
+)
+
+func (a App) Create(c echo.Context) error {
+	newClient, err := models.ValidateRegisterClientRequest(c)
+	if err != nil {
+		return c.JSON(err.Code, err)
+	}
+
+	client, err := a.clientsUseCases.CreateClient(newClient)
+	if err != nil {
+		return c.JSON(err.Code, err)
+	}
+
+	return c.JSON(http.StatusCreated, client)
+}
+
+func (a App) FindById(c echo.Context) error {
+	id := c.Param("id")
+
+	client, err := a.clientsUseCases.FindById(id)
+	if err != nil {
+		return c.JSON(err.Code, err)
+	}
+
+	return c.JSON(http.StatusFound, client)
+}
