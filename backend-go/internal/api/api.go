df3d9276a8ecaa44f9ed3a31831340468e6ca095
Gabriel Borel <biel_borel@hotmail.com>
2023-01-08 20:57:18 -0300

    rotas atualizadas e repositorio instanciado

--- backend-go/internal/api/api.go
+++ backend-go/internal/api/api.go
@@ -1,21 +1,33 @@
 package api
 
 import (
+	"github.com/gabrielborel/desafio-sharenergy-2023-01/backend-go/pkg/config"
+	"github.com/gabrielborel/desafio-sharenergy-2023-01/backend-go/pkg/data"
+	"github.com/gabrielborel/desafio-sharenergy-2023-01/backend-go/pkg/usecases"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type App struct {
-	server *echo.Echo
+	server          *echo.Echo
+	clientsUseCases usecases.IClientsUseCases
+	cfg             *config.Settings
 }
 
-func New() *App {
+func New(cfg *config.Settings, client *mongo.Client) *App {
 	server := echo.New()
 	server.Use(middleware.Recover())
-	return &App{server}
+
+	clientsRepo := data.NewClientsRepository(cfg, client)
+	clientsUseCase := usecases.NewClientsUseCases(cfg, clientsRepo)
+
+	return &App{server, clientsUseCase, cfg}
 }
 
 func (a App) ConfigureRoutes() {
+	a.server.POST("/clients", a.Create)
+	a.server.GET("/clients/:id", a.FindById)
 }
 
 func (a App) Start() {
