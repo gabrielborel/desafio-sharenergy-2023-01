3118bdffdf67f21b75236ce476c458d63b760bdc
Gabriel Borel <biel_borel@hotmail.com>
2023-01-10 02:01:02 -0300

    testes de atualizar cliente criado

---
+++ backend-node/src/modules/clients/use-cases/update-client/update-client-controller.spec.ts
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { app } from "../../../../app";
+import { Client } from "../../entities/client";
+import { AuthenticateUseCase } from "../../../authentication/use-cases/authenticate/authenticate-use-case";
+
+describe("Update Client Controller", () => {
+  let mongoServer: MongoMemoryServer;
+  let authenticateUseCase: AuthenticateUseCase;
+  let accessToken: string;
+
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    mongoose.connect(mongoServer.getUri());
+    authenticateUseCase = new AuthenticateUseCase();
+
+    const { token } = await authenticateUseCase.execute({
+      username: "desafiosharenergy",
+      password: "sh@r3n3rgy",
+    });
+    accessToken = token;
+  });
+
+  afterAll(async () => {
+    if (mongoServer) {
+      await mongoose.connection.dropDatabase();
+      await mongoose.connection.close();
+      await mongoServer.stop();
+    }
+  });
+
+  afterEach(async () => {
+    if (mongoServer) {
+      const collections = await mongoose.connection.db.collections();
+      for (let collection of collections) {
+        await collection.drop();
+      }
+    }
+  });
+
+  const clientData = new Client({
+    name: "Gabriel Borel",
+    email: "biel_borel@hotmail.com",
+    address: "Volta Redonda, Rj",
+    cellphone: "24999899256",
+    cpf: "19252283714",
+  });
+
+  it("should be able to update a client", async () => {
+    const { body: createdClient } = await request(app)
+      .post("/clients")
+      .send({ ...clientData })
+      .set({ Authorization: `Bearer ${accessToken}` });
+
+    const newClient = new Client({
+      name: "Gabriel Jonas",
+      email: "borel2219@hotmail.com",
+      address: "Volta Redonda, Rj",
+      cellphone: "24999899256",
+      cpf: "19252283714",
+    });
+
+    const { body: updatedClient } = await request(app)
+      .put(`/clients/${createdClient.id}`)
+      .send({ ...newClient })
+      .set({ Authorization: `Bearer ${accessToken}` });
+
+    expect(updatedClient.name).toBe(newClient.name);
+    expect(updatedClient.email).toBe(newClient.email);
+  });
+});
